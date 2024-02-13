f99a9b9e245e00a553a748336201cbab8794debc
RavaszJan <janravasz@gmail.com>
2024-02-13 19:02:44 +0100

    Abstrakcia

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # abstraktne triedy
+from abc import ABC, abstractmethod
+class Zviera(ABC):
+    @abstractmethod
+    def zvuk(self):
+        pass
+
+class Pes(Zviera):
+    def zvuk(self):
+        return "Haf"
+
+class Macka(Zviera):
+    def zvuk(self):
+        return "Mnau"
+
+# moje_zviera=Zviera()
+
+moj_pes=Pes()
+print(moj_pes.zvuk())
+
+moja_macka=Macka()
+print(moja_macka.zvuk())
+
